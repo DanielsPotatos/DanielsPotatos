ca64b1df1431f6db571764510a7febdc083f1711
Daniel Tiourine <90714623+DanielsPotatos@users.noreply.github.com>
2021-11-23 16:55:36 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,7 @@
-import './style.css'
-
+import "../styles/style.css";
+import { merchandise } from "./merchandise";
 document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+`;
+console.log(merchandise);
