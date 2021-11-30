88ad6022511a0dc25e895b26817c827fe266723d
Daniel Tiourine <90714623+DanielsPotatos@users.noreply.github.com>
2021-11-30 14:10:35 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -10,18 +10,12 @@ document.querySelector(".btn").addEventListener("click", function () {
   if (document.body.classList.contains("cool")) {
     document.body.classList.add("warm");
     document.body.classList.remove("cool");
-  } else {
-    document.body.classList.add("cool");
-    document.body.classList.remove("warm");
-  }
+  } 
 });
 
 document.querySelector(".btn1").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
-    document.body.classList.remove("cool");
-  } else {
+  if (document.body.classList.contains("warm")) {
     document.body.classList.add("cool");
     document.body.classList.remove("warm");
-  }
+  } 
 });
