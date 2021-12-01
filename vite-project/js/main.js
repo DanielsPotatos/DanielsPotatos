78abfd5c8f331ee6c60583b483c70c9bbb36b894
Daniel Tiourine <90714623+DanielsPotatos@users.noreply.github.com>
2021-12-01 13:22:26 -0500

    asassasas

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,21 +1,20 @@
 import "../styles/style.css";
 import { merchandise } from "./merchandise";
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  
+  
 `;
 console.log(merchandise);
 
 document.querySelector(".btn").addEventListener("click", function () {
-  if (document.body.classList.contains("cool")) {
-    document.body.classList.add("warm");
+     document.body.classList.add("warm");
     document.body.classList.remove("cool");
-  } 
+   
 });
 
 document.querySelector(".btn1").addEventListener("click", function () {
-  if (document.body.classList.contains("warm")) {
+ 
     document.body.classList.add("cool");
     document.body.classList.remove("warm");
-  } 
+   
 });
