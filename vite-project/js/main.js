7c46411f3f11e09ad3d5cd1c42086417e97495bb
DanielsPotatos <JOnathanw26@nycstudents.net>
2021-12-06 22:04:15 -0500

    ssss

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -1,6 +1,5 @@
 import "../styles/style.css";
 import { merchandise } from "./merchandise";
-
 console.log(merchandise);
 
 document.querySelector(".btn").addEventListener("click", function () {
@@ -15,24 +14,26 @@ document.querySelector(".btn1").addEventListener("click", function () {
     document.body.classList.remove("warm");
    
 });
-
 const DOMSelector = {
-warm:document.getElementById("btn"),
-dark:document.getElementById("btn1"),
-
+  warm: document.getElementById("btn"),
+  dark: document.getElementById("btn1"),
+  cheap: document.getElementById("btn2"),
+  expensive: document.getElementById("btn3"),
+  buttons: document.querySelectorAll("button"),
+  parent: document.querySelector(".parent"),
 };
 
 
 function cardCreator(array) {
   merchandise.forEach((pokemon) => {
-    pkms.parent.insertAdjacentHTML(
+    DOMSelector.parent.insertAdjacentHTML(
       "beforeend",
       `<div class="child">
         <img class="display-img" src="${pokemon.img}"/>
         <h2 class="display-name">${pokemon.name}</h2>
         <h3 class="display-price">${pokemon.price}</h3>
   </div> `
-    );
-  });
-}
+  );
+    });
+  }
 cardCreator();
--- vite-project/js/merchandise.js
+++ vite-project/js/merchandise.js
@@ -3,28 +3,27 @@ const merchandise = [
     name:"Pikachu",
     price: "$2.0",
     img: "https://cdn.vox-cdn.com/thumbor/eFEHo8eygHajtwShwT9e_jf7c-c=/0x0:1920x1080/1200x800/filters:focal(722x227:1028x533)/cdn.vox-cdn.com/uploads/chorus_image/image/69323002/Screen_Shot_2021_05_21_at_9.54.00_AM.0.jpeg",
-    inStock: true
+    
 },
 {   
     name:"Raichu",
     price: "$4.0",
-    img: "https://static.wikia.nocookie.net/pokemon/images/4/4b/Goh_Raichu.png/revision/latest?cb=20200906115835",
-    inStock: true
+    img: "https://archives.bulbagarden.net/media/upload/thumb/3/37/Visquez_Raichu.png/250px-Visquez_Raichu.png",
+   
 },
 {   name:"Charmander",
     price: "$7.0",
     img: "https://static1.thegamerimages.com/wordpress/wp-content/uploads/2020/05/pokemon-charmander-change_1400x700.jpg",
-    inStock: true
+    
 },
 {   name:"Squirtle",
     price: "$9.0",
-    img: "https://static.wikia.nocookie.net/leonhartimvu/images/8/8c/Tierno_Squirtle.png/revision/latest?cb=20190128082527",
-    inStock: true
+    img: "https://archives.bulbagarden.net/media/upload/thumb/8/8c/Tierno_Squirtle.png/250px-Tierno_Squirtle.png",
 },
 {   name:"Bulbasaur",
     price: "$12.0",
     img: "https://cdn1.dotesports.com/wp-content/uploads/2019/11/14155606/e18132a4c3cce8b8c85c87b420ce67dc.jpg",
-    inStock: true
+    
 },
 {   name:"Magikarp",
     price: "$1.0",
@@ -34,17 +33,17 @@ const merchandise = [
 {   name:"Eevee",
     price: "$15.0",
     img: "https://cdn.vox-cdn.com/thumbor/_u-nTeKGiucoLwyelHQock4mqsQ=/0x0:800x450/1200x800/filters:focal(239x138:367x266)/cdn.vox-cdn.com/uploads/chorus_image/image/62348289/800px-Serena_Eevee.0.0.png",
-    inStock: false
+    
 },
 {   name:"Ghastly",
     price: "$6.0",
     img: "https://cdn.gamer-network.net/2019/usgamer/ghastly_featured.jpg/EG11/thumbnail/1920x1080/format/jpg/quality/65/this-ghastly-gastly-is-already-2019s-pokemon-pumpkin-of-the-year.jpg",
-    inStock: true
+    
 },
 {   name:"Dratini",
     price: "$25.0",
     img: "https://cdn1.dotesports.com/wp-content/uploads/2020/10/23081620/dratini-crown-tundra.jpg",
-    inStock: false
+    
 },
 ];
 
