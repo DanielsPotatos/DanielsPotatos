80a2a183edadfd652d1e951f58176bd78e3f84a8
DanielsPotatos <JOnathanw26@nycstudents.net>
2021-12-08 17:37:38 -0500

    Update main.js

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,18 +2,6 @@ import "../styles/style.css";
 import { merchandise } from "./merchandise";
 console.log(merchandise);
 
-document.querySelector(".btn").addEventListener("click", function () {
-     document.body.classList.add("warm");
-    document.body.classList.remove("cool");
-   
-});
-
-document.querySelector(".btn1").addEventListener("click", function () {
- 
-    document.body.classList.add("cool");
-    document.body.classList.remove("warm");
-   
-});
 const DOMSelector = {
   warm: document.getElementById("btn"),
   dark: document.getElementById("btn1"),
@@ -23,31 +11,56 @@ const DOMSelector = {
   parent: document.querySelector(".parent"),
 };
 
+document.querySelector(".btn").addEventListener("click", function () {
+  document.body.classList.add("warm");
+ document.body.classList.remove("cool");
 
-function cardCreator(array) {
-  merchandise.forEach((pokemon) => {
-    DOMSelector.parent.insertAdjacentHTML(
-      "beforeend",
-      `<div class="child">
-        <img class="display-img" src="${pokemon.img}"/>
-        <h2 class="display-name">${pokemon.name}</h2>
-        <h3 class="display-price">${pokemon.price}</h3>
-  </div> `
+});
+
+document.querySelector(".btn1").addEventListener("click", function () {
+
+ document.body.classList.add("cool");
+ document.body.classList.remove("warm");
+
+});
+
+
+
+
+function clear() {
+  DOMSelector.parent.innerHTML = "";
+}
+function adder(pokemon) {
+  DOMSelector.parent.insertAdjacentHTML(
+    "beforeend",
+    `<div class="child">
+    <img class="display-img" src="${pokemon.img}"/>
+    <h2 class="display-name">${pokemon.name}</h2>
+    <h3 class="display-price">${pokemon.price}</h3>
+</div> `
   );
-    });
-  }
+}
+
+function cardCreator() {
+  merchandise.forEach((mon) => {
+    adder(mon);
+  });
+}
 cardCreator();
 
 
 document.querySelector(".btn2").addEventListener("click", function () {
-  const potato = merchandise.filter(pokemon => pokemon.price <= 10.0)
-   
-  console.log(potato);
- });
- 
- document.querySelector(".btn3").addEventListener("click", function () {
-   const potato2 = merchandise.filter(pokemon => pokemon.price >= 10.0)
-    
-   console.log(potato2);
+  clear();
+  const cheap = merchandise.filter((merchandise) => merchandise.price <= 10);
+  cheap.forEach((mon) => {
+    adder(mon);
   });
-   
+});
+
+document.querySelector(".btn3").addEventListener("click", function () {
+  clear();
+  const expensive = merchandise.filter((merchandise) => merchandise.price >= 10);
+  expensive.forEach((mon) => {
+    adder(mon);
+  });
+});
